a45a5b7fd545824e8d8a7b2ce4e53fc94c3abfb0
moonstripe <kojinglick@gmail.com>
2022-08-01 22:57:28 -0700

    random button padding

--- islands/RandomPost.tsx
+++ islands/RandomPost.tsx
@@ -10,7 +10,7 @@ export default ({ currentSlug, postList }: RandomPost) => {
 
     return (
         <a href={`${otherPosts[random].slug.slice(otherPosts[random].slug, -3)}`}>
-            <button class={tw`my-8 w-full rounded border-solid border-2 dark:border-green-400 dark:text-green-300 columns-1`}>
+            <button class={tw`my-8 py-2 w-full border-black rounded border-solid border-2 dark:border-green-400 dark:text-green-300 columns-1`}>
                 {`${otherPosts[random].title.slice(1).trim()} - ${otherPosts[random].date.slice(otherPosts[random].date.indexOf(',')+2, otherPosts[random].date.length)}`}
             </button>
         </a>
