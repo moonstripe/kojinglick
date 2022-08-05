6bb1eb6ff74058c5a7336ba1042da9c0df853d65
moonstripe <kojinglick@gmail.com>
2022-08-04 22:26:29 -0700

    fixed home

--- routes/index.tsx
+++ routes/index.tsx
@@ -96,7 +96,7 @@ export default ({ data, url }: PageProps) => {
                     <div class={tw`my-3`}>
                         <a class={tw`my-3`} href={`mailto:kojinglick@gmail.com`} target="_blank" rel="noopener noreferrer">
                             <div class={tw`flex flex-row`}>
-                                <svg class={tw`my-auto mr-5`} viewBox="0 0 24 24" width="24" fill="#ff0000" >
+                                <svg class={tw`my-auto mr-5`} viewBox="0 0 24 24" width="24px" fill="#ff0000" >
                                     <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"></path>
                                 </svg>
                                 <h1 class={tw`my-auto align-text-middle text-2xl text-gray-700 dark:text-green-200`}>kojinglick@gmail.com</h1>
