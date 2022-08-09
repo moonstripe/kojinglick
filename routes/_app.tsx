64fe54f426fc1856027eccade05548ecd870ca4b
moonstripe <kojinglick@gmail.com>
2022-08-08 20:41:02 -0700

    bye

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,8 +7,6 @@ export default function App(props: any) {
   return (
     <Fragment>
       <Head>
-        {/* <title>Kojin Glick</title>
-        <meta name="description" content="Kojin's world!" /> */}
       </Head>
       <props.Component />
     </Fragment>
