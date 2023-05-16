819937ea6f3d89e195c53f8b108eaa48ef62f5d5
moonstripe <kojinglick@gmail.com>
2023-05-16 10:47:25 -0700

    new works

--- routes/about.tsx
+++ routes/about.tsx
@@ -28,11 +28,11 @@ export default (props: PageProps) => {
                 <h1 class={tw`text-4xl dark:text-green-200`}>Bio</h1>
 
                 <ul class={tw`list-disc ml-4 mb-4 mt-4 list-none`}>
-                    
+
                     <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl`}>I run <a href='https://www.moonstripe.com' target="_blank" class={tw`text-black dark:text-neutral`}>moonstripe design</a>.</li>
 
                     <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl`}>I am a Master's Candidate in <a href='https://www.middlebury.edu/institute/academics/degree-programs/nonproliferation-terrorism-studies' target="_blank" class={tw`text-black dark:text-neutral`}>Non-Proliferation and Terrorism Studies</a> at <a href='https://www.middlebury.edu/institute/' target="_blank" class={tw`text-black dark:text-neutral`}>Middlebury Institute for International Studies at Monterey</a>.</li>
-                   
+
                     <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl ml-4`}>I am a Graduate Research Assistant at the <a href='https://www.middlebury.edu/institute/academics/centers-initiatives/ctec' target="_blank" class={tw`text-black dark:text-neutral`}>Center on Terrorism, Extremism, and Counter-Terrorism</a>.</li>
 
                     <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl ml-4`}>I am a Spring 2023 <a href='https://www.middlebury.edu/institute/academics/centers-initiatives/ctec/michael-donnelly-ctec-research-fellowship' target="_blank" class={tw`text-black dark:text-neutral`}>Michael Donnelly Fellow</a>.</li>
@@ -45,7 +45,14 @@ export default (props: PageProps) => {
                 </ul>
 
                 <h1 class={tw`text-xl dark:text-green-200 text-4xl`}>Works</h1>
-                <p class={tw`mb-4 text-xl text-gray-600 dark:text-green-400 mt-4`}>I'll add projects I'm proud of here.</p>
+                <ul class={tw`list-disc ml-4 mb-4 mt-4 list-none`}>
+
+                    <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl`}>March, 2023: <a href='https://www.middlebury.edu/institute/academics/centers-initiatives/ctec/ctec-publications/changing-face-wagner-group-military' target="_blank" class={tw`text-black dark:text-neutral`}>The Changing Face of the Wagner Group: From Military Adventurism to Venture Capitalism</a></li>
+                    <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl ml-4`}>This paper examines the emergence of Prigozhin's Wagner Group and its evolution from conducting security operations in Syria and Africa to establishing a modern business center in St. Petersburg, providing insights into its role in advancing Russia's foreign policy goals.</li>
+
+                    <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl`}>March, 2023: <a href='https://www.middlebury.edu/institute/academics/centers-initiatives/ctec/ctec-publications/wagner-groups-social-footprint-time-series' target="_blank" class={tw`text-black dark:text-neutral`}>The Wagner Group’s Social Footprint: A Time-Series Sentiment Analysis of PMC World</a></li>
+                    <li class={tw`text-gray-600 dark:text-green-400 mb-2 text-xl ml-4`}>This paper explores the changing dynamics of conflict actors' engagement with domestic audiences, focusing on Prigozhin and the Wagner Group. It utilizes statistical methods and sentiment analysis of a VK channel to highlight the significance of their public orientation and reveals Prigozhin's increasing influence in Russian domestic affairs and potential impact on broader foreign policy, including the Russo-Ukrainian war.</li>
+                </ul>
             </Layout>
         </div>
     );
